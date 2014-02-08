d3a851b2ed0dd6ab4803952175c438fd708788e0
Mikko Oksa <m3tacity@gmail.com>
2014-02-08 02:27:51 +0200

    Really fix them indentations..

--- p010/p10.cpp
+++ p010/p10.cpp
@@ -6,33 +6,33 @@
 using namespace std;
 
 vector<unsigned int> givePrimes(unsigned int n) {
-		vector<unsigned int> primes;
-		vector<bool> arePrimes(n + 1, true);
+	vector<unsigned int> primes;
+	vector<bool> arePrimes(n + 1, true);
 
-		for (unsigned int i = 2; i < n + 1; ++i) {
-				if (arePrimes[i]) {
-						primes.push_back(i);
-						for (unsigned int j = 2 * i; j < n + 1; j += i) {
-								arePrimes[j] = false;
-						}
-				}
+	for (unsigned int i = 2; i < n + 1; ++i) {
+		if (arePrimes[i]) {
+			primes.push_back(i);
+			for (unsigned int j = 2 * i; j < n + 1; j += i) {
+				arePrimes[j] = false;
+			}
 		}
+	}
 
-		return primes;
+	return primes;
 }
 
 int main() {
-		unsigned int n;
-		cout << "Primes between 1-N. Give N: ";
-		cin >> n;
+	unsigned int n;
+	cout << "Primes between 1-N. Give N: ";
+	cin >> n;
 
-		vector<unsigned int> primes = givePrimes(n);
-		long long int primeSum = 0LL;
-		for (unsigned int i = 0; i < primes.size(); ++i) {
-				//cout << i+1 << ": " << primes[i] << endl;
-				primeSum += primes[i];
-		}
-		cout << primeSum << endl;
+	vector<unsigned int> primes = givePrimes(n);
+	long long int primeSum = 0LL;
+	for (unsigned int i = 0; i < primes.size(); ++i) {
+		//cout << i+1 << ": " << primes[i] << endl;
+		primeSum += primes[i];
+	}
+	cout << primeSum << endl;
 
-		return 0;
+	return 0;
 }
